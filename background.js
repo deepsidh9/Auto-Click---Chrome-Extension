c5c286cc7a5b82b5a3387255f88b66d400a6d2b8
deepsidh9 <deep.barca@gmail.com>
2020-03-22 23:50:17 +0530

    Initial Commit

---
+++ background.js
@@ -0,0 +1,106 @@
+function getElementbyXpath(xpath) {
+
+    return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext();
+
+}
+
+// Called when the user clicks on the browser action.
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+
+    });
+
+});
+
+chrome.runtime.onMessage.addListener(
+
+    function (request, sender, sendResponse) {
+
+        if (request.message === "open_new_tab") {
+
+            console.log("Background.js logging : Message received from content.js",request.message);
+
+        }
+    }
+);
+
+// chrome.webNavigation.onCompleted.addListener(function () {
+
+//     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+//         console.log(tabs[0].url);
+//         var activeTab = tabs[0];
+
+//         if (activeTab.url.includes("return_to")) {
+
+//             console.log("Background.js logging : sending message to content.js")
+//             chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_submit" });
+
+//         }
+
+//         else if (activeTab.url==="https://github.com/") {
+
+//             console.log("Background.js logging : sending message to content.js")
+//             chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_single_sign_on" });
+
+//         }
+//     });
+
+// });
+
+
+chrome.webNavigation.onCompleted.addListener(function() {
+
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+        console.log(tabs[0].url);
+        var activeTab = tabs[0];
+
+        if (activeTab.url.includes("return_to") && activeTab.url.includes("nutanix.okta.com/app/githubcloud/")) {
+
+            console.log("Background.js logging : sending message to content.js")
+            chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_submit" });
+
+        }
+
+        else if (activeTab.url==="https://github.com/") {
+
+            console.log("Background.js logging : sending message to content.js")
+            chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_single_sign_on" });
+
+        }
+    });
+
+
+});
+
+
+// chrome.tabs.onUpdated.addListener(function (tabId , info) {
+
+//     if (info.status === 'complete') {
+
+//         chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+
+//             console.log(tabs[0].url);
+//             var activeTab = tabs[0];
+    
+//             if (activeTab.url.includes("return_to")) {
+    
+//                 console.log("Background.js logging : sending message to content.js")
+//                 chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_submit" });
+    
+//             }
+    
+//             // else if (activeTab.url==="https://github.com/") {
+    
+//             //     console.log("Background.js logging : sending message to content.js")
+//             //     chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_single_sign_on" });
+    
+//             // }
+//         });
+//     }
+//   });
---
+++ content.js
@@ -0,0 +1,59 @@
+function getElementbyXpath(xpath) {
+
+    return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext();
+}
+
+function getAllSingleSignOnLinks(links){
+    var signOnLinks=[];
+    for(var i = 0; i < links.length; i++) {
+
+        if(links[i].text.includes("Single sign-on")){
+            signOnLinks.push(links[i].href);
+        }
+        
+      }
+      return signOnLinks;
+}
+
+function clickOnSingleSignOn(signOnLinks) {
+    
+    console.log("Content.js logging : Message Received")
+    window.location.href=signOnLinks[0]
+
+
+    // var singleSignOnLink = getElementbyXpath('/html/body/div[4]/div/div/div/main/div/div/div/div[3]/div/ul/li[1]/p/a')
+    // singleSignOnLink.click();
+}
+
+function clickOnSubmit() {
+
+    var submitButton = getElementbyXpath('/html/body/div[3]/main/div/div[2]/form/button');
+    submitButton.click();
+}
+
+chrome.runtime.onMessage.addListener(
+
+    function (request, sender, sendResponse) {
+
+        console.log("Content.js logging : Message Received")
+
+        if (request.message === "clicked_browser_action" || request.message === "click_on_single_sign_on") {
+
+            var links = document.links;
+            signOnLinks=getAllSingleSignOnLinks(links)
+            if (signOnLinks.length>0){
+                clickOnSingleSignOn(signOnLinks);
+                chrome.runtime.sendMessage({ "message": "Clicked on Single Sign On" });
+            }
+            
+
+        }
+
+        else if (request.message === "click_on_submit") {
+
+            clickOnSubmit()
+            chrome.runtime.sendMessage({ "message": "Clicked on Submit" });
+
+        }
+    }
+);
