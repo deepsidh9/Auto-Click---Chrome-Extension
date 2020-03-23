645138f43c9259db9703f388e39f6e83901ed488
deepsidh9 <deep.barca@gmail.com>
2020-03-23 14:35:28 +0530

    Handled cases for element in functions

--- background.js
+++ background.js
@@ -10,13 +10,13 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-        chrome.storage.local.set({"do_operations": "true"}, function() {
+        chrome.storage.local.set({ "do_operations": "true" }, function () {
             console.log('do_operations is set to ' + "true");
-          });
+        });
 
         var activeTab = tabs[0];
-        console.log("Active tab as",activeTab)    
-        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" , "do_operations":"true"});
+        console.log("Active tab as", activeTab)
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action", "do_operations": "true" });
 
     });
 
@@ -26,44 +26,43 @@ chrome.runtime.onMessage.addListener(
 
     function (request, sender, sendResponse) {
 
-        console.log("Background.js logging : Message received from content.js as ",request.message);
+        console.log("Background.js logging : Message received from content.js as ", request.message);
 
         if (request.message === "change_do_operations") {
 
-            chrome.storage.local.set({"do_operations": "false"}, function() {
+            chrome.storage.local.set({ "do_operations": request.do_operations }, function () {
 
-                console.log('do_operations is set to ' + "false");
-              });    
+                console.log('do_operations is set to ' + request.do_operations);
+            });
         }
     }
 );
 
-chrome.webNavigation.onCompleted.addListener(function() {
+chrome.webNavigation.onCompleted.addListener(function () {
 
     chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 
-        console.log("On this tab :",tabs[0].url);
+        console.log("On this tab :", tabs[0].url);
         var activeTab = tabs[0];
 
-        chrome.storage.local.get(['do_operations'], function(result) {
+        chrome.storage.local.get(['do_operations'], function (result) {
             console.log(' Web navigation completed ; Value of do_operations currently is ' + result.do_operations);
-          });
+        });
 
-        if (activeTab.url.includes("return_to") && activeTab.url.includes("github.com/orgs") && result.do_operations==="true") 
-        {
+        if (activeTab.url.includes("return_to") && activeTab.url.includes("github.com/orgs") && result.do_operations === "true") {
             console.log("On submit page")
             console.log("Background.js logging : sending message to content.js to click on submit")
             chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_submit" });
 
         }
 
-        else if (activeTab.url==="https://github.com/" && result.do_operations==="true") {
+        else if (activeTab.url === "https://github.com/" && result.do_operations === "true") {
             console.log("On main page")
             console.log("Background.js logging : sending message to content.js  to click on single sign on")
             chrome.tabs.sendMessage(activeTab.id, { "message": "click_on_single_sign_on" });
 
         }
-        else{
+        else {
             console.log("Doing Nothing")
         }
     });
--- content.js
+++ content.js
@@ -3,23 +3,21 @@ function getElementbyXpath(xpath) {
     return document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null).iterateNext();
 }
 
-function getAllSingleSignOnLinks(links){
-    var signOnLinks=[];
-    for(var i = 0; i < links.length; i++) {
+function getAllSingleSignOnLinks(links) {
 
-        if(links[i].text.includes("Single sign-on")){
+    var signOnLinks = [];
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].href.includes("sso")) {
+            console.log("Pushing Links as", links[i].href)
             signOnLinks.push(links[i].href);
-            console.log("Pushing Links as",links[i].href)
+            return signOnLinks
         }
-        console.log("Returning link as ",signOnLinks)
-        return signOnLinks;
-      }
-}
+    }
 
 function clickOnSingleSignOn(signOnLinks) {
-    
-    console.log("Setting window location href as ",signOnLinks[0])
-    window.location.href=signOnLinks[0]
+
+    console.log("Setting window location href as ", signOnLinks[0])
+    window.location.href = signOnLinks[0]
 }
 
 function clickOnSubmit() {
@@ -28,64 +26,44 @@ function clickOnSubmit() {
     submitButton.click();
 }
 
+function handleSignonLinks(signOnLinks) {
+    if (signOnLinks.length > 0) {
 
-function waitForElementToDisplay(selector, time) {
-    console.log("Waiting for element")
-    if(document.getElementsByClassName(selector)!=null) {
-        console.log("Element found")
-        var links = document.links;
-        console.log("Total Links Length as",links.length)
-
-            // signOnLinks=getAllSingleSignOnLinks(links)
-            
-            
-            var signOnLinks=[];
-            for(var i = 0; i < links.length; i++) {
-        
-                if(links[i].href.includes("sso")){
-                    signOnLinks.push(links[i].href);
-                    console.log("Pushing Links as",links[i].href)
-                    
-        
-                }
-                }
-                
-                try{
-                    if (signOnLinks.length>0 ){
-
-                        console.log("signOnLinks array is as",signOnLinks)
-            if (signOnLinks.length>0){
-                clickOnSingleSignOn(signOnLinks);
-                chrome.runtime.sendMessage({ "message": "Clicked on Single Sign On" });
-            }
-
-                    }    
-                }
-
-            catch{
-                setTimeout(console.log("waiting for new links to come"),5000);
-                var signOnLinks=[];
-            for(var i = 0; i < document.links.length; i++) {
-                if(links[i].href.includes("sso")){
-                    signOnLinks.push(document.links[i].href);
-                    console.log("Pushing Links as",document.links[i].href)
-                }
-                }
-                if (signOnLinks.length>0){
-                    clickOnSingleSignOn(signOnLinks);
-                    chrome.runtime.sendMessage({ "message": "Clicked on Single Sign On" });
-                }
-                else{
-                    console.log("Could not find any single sign on links,stopping operations")
-                    chrome.runtime.sendMessage({ "message": "change_do_operations","do_operations":"false" });
-                }
-                }
-
-            
+        console.log("signOnLinks array is as", signOnLinks)
+        if (signOnLinks.length > 0) {
+            clickOnSingleSignOn(signOnLinks);
+            chrome.runtime.sendMessage({ "message": "Clicked on Single Sign On" });
+        }
+        else {
+            console.log("Could not find any single sign on links,stopping operations")
+            chrome.runtime.sendMessage({ "message": "change_do_operations", "do_operations": "false" });
+        }
 
     }
+}
+
+function waitForElementToDisplay(selector, time) {
+
+    console.log("Waiting for element")
+    if (document.getElementsByClassName(selector) != null) {
+
+        console.log("Element found")
+        var links = document.links;
+        console.log("Total Links Length as", links.length)
+
+        signOnLinks = getAllSingleSignOnLinks(links)
+
+        try {
+            handleSignonLinks(signOnLinks)
+        }
+        catch{
+            window.setTimeout(console.log("waiting for new links to come"), 5000);
+            signOnLinks = getAllSingleSignOnLinks(document.links)
+            handleSignonLinks()
+        }
+    }
     else {
-        setTimeout(function() {
+        setTimeout(function () {
             waitForElementToDisplay(selector, time);
         }, time);
     }
@@ -96,20 +74,19 @@ chrome.runtime.onMessage.addListener(
 
     function (request, sender, sendResponse) {
 
-        console.log("Content.js logging : Message Received",(request.message,request.do_operations))
+        console.log("Content.js logging : Message Received", (request.message, request.do_operations))
 
         if ((request.message === "clicked_browser_action" || request.message === "click_on_single_sign_on") &&
-            request.do_operations==="true")
-         {
+            request.do_operations === "true") {
             console.log("Sleeping for 5 seconds")
 
-            window.setTimeout(function() {
+            window.setTimeout(function () {
                 waitForElementToDisplay("notes", 5000);
-              }, 5000);
+            }, 5000);
         }
 
-        else if (request.message === "click_on_submit") {
-            console.log("Clicking on submit")    
+        else if (request.message === "click_on_submit" && request.do_operations === "true") {
+            console.log("Clicking on submit")
             clickOnSubmit()
             chrome.runtime.sendMessage({ "message": "Clicked on Submit" });
         }
